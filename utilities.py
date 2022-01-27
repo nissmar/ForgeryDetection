473e381b9e6c6f91965cfb694c8306a9a8468815
nissmar <maruani.nissim@gmail.com>
2022-01-27 14:27:40 +0100

    affinity fakes

--- utilities.py
+++ utilities.py
@@ -1,13 +1,18 @@
 from PIL import Image
 import numpy as np
+import skimage.measure
 
 
 def show_heat_map(im, mask):
-    '''combines bw image ([0,1]) with mask ([0,1])'''
-    mask = Image.fromarray((255*mask).astype('uint8'))
-    mask = np.array(mask.resize((im.shape[1], im.shape[0])))/255
+    """combines bw image ([0,255]) with mask ([0,1])"""
+    mask = Image.fromarray((255 * mask).astype("uint8"))
+    mask = np.array(mask.resize((im.shape[1], im.shape[0]))) / 255
 
-    res = np.repeat(im[:, :, np.newaxis], 3, axis=2)/255
-    res[:, :, 1] = res[:, :, 0]*(1-mask)
-    res[:, :, 2] = res[:, :, 0]*(1-mask)
+    res = np.repeat(im[:, :, np.newaxis], 3, axis=2) / 255
+    res[:, :, 1] = res[:, :, 0] * (1 - mask)
+    res[:, :, 2] = res[:, :, 0] * (1 - mask)
     return res
+
+
+def reduce_array(arr, filter, method=np.max):
+    return skimage.measure.block_reduce(arr, (filter, filter), method)
